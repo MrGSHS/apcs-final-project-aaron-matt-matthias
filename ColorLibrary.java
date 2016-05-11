1ba77af49c638eebe215b3d1b7ae7dbb9b02ea12
mkaprau7 <mkaprau7@students.d125.org>
2016-05-11 15:20:05 -0500

    Add files via upload

---
+++ ColorLibrary.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class ColorLibrary
+{
+    public static Color lightGray()
+    {
+        return new Color(200, 200, 200);
+    }
+    
+    public static Color lightBlue()
+    {
+        return new Color(100, 100, 200);
+    }
+}
